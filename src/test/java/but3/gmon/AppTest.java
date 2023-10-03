67c11414d80516778514c902293e5deb23dd300b
CacheCaille <zoolk.gabi@gmail.com>
2023-10-03 17:28:09 +0200

    First commit

---
+++ src/test/java/but3/gmon/AppTest.java
@@ -0,0 +1,20 @@
+package but3.gmon;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
