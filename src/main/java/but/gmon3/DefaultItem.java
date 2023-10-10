ec71bd856ef0f0da1df88283f50c67387cd7ec65
CacheCaille <zoolk.gabi@gmail.com>
2023-10-10 14:20:26 +0200

    Ajout de l'autheur

--- src/main/java/but/gmon3/DefaultItem.java
+++ src/main/java/but/gmon3/DefaultItem.java
@@ -2,6 +2,7 @@ package but.gmon3;
 
 /**
  * Implémentation de l'interface Item.
+ * @author Monczewski Gabriel dit le Beau
  */
 public class DefaultItem implements Item {
 	// TODO Finir la classe
